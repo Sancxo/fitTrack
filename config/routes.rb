3914cbff2ad87823d5241a9ec3a79cc1cd7b8f4e
Simon <simon@7lieues.io>
2022-04-03 00:05:31 +0200

    about page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'site#home'
+  get 'site/about'
   # get 'site/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
