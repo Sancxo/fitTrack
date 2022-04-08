6614b961071d52d9defae2c29522719faea31c31
Simon <simon@7lieues.io>
2022-04-08 16:27:11 +0200

    finishing installing Bootstrap + first styles in nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def set_active_class(path)
+        current_page?(path) ? "active" : ""
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "site#home"
-  # get 'site/home'
-  get 'site/about'
+  get '/about' => "site#about"
+
   resources :activities
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
