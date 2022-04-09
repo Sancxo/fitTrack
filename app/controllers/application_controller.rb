da72340d247b6616813172990fa6b3a6daef68f9
Simon <simon@7lieues.io>
2022-04-09 23:14:14 +0200

    start configuring Devise with turbo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true
+    protect_from_forgery with: :exception, prepend: true
+
+    before_action :config_permitted_params, if: :devise_controller?
+
+    protected
+
+    def config_permitted_params
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:first_name, :last_name, :birthdate, :about, :email, :password, :password_confirmation )
+        end
+    end
 end
