5b5c8c553998fddcaa694e1f17eee28be971d57f
Sancxo <simon.tirant@gmail.com>
2022-04-23 14:46:01 +0200

    adding dropdown to navbar with new set_active_class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 # Dynamically adds & removes "active" class to the link in the navbar
 module ApplicationHelper
     def set_active_class(path)
-        current_page?(path) ? "active" : ""
+        if path.is_a? Array
+            if path.any? {|p| current_page?(p)}
+                "active"
+            else
+                "" 
+            end
+        else
+            current_page?(path) ? "active" : ""            
+        end
     end
 end
