8dd806297b1d622311f2c3937532276729c8585b
Sancxo <simon.tirant@gmail.com>
2023-08-04 13:18:33 +0200

    build: renames migration file to correspond with class name

--- db/migrate/20220408144439_change_datatype_for_time.rb
+++ db/migrate/20220408144439_change_datatype_for_time.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForDateAndAddHour < ActiveRecord::Migration[7.0]
+class ChangeDatatypeForTime < ActiveRecord::Migration[7.0]
   def change
     change_table :activities do |t|
       t.time :time
